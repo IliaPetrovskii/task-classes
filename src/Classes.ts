c03fec67603e27301b01a1ea778de9a38a99c2ae
Ilia Petrovskii <ipetrovskii1301@gmail.com>
2022-10-25 13:54:53 +0300

    Classes

--- src/Classes.ts
+++ src/Classes.ts
@@ -5,4 +5,17 @@
 - Класс должен иметь метод increment, который увеличивает counter на 1 и возвращает результат
 - Класс должен иметь метод decrement, который уменьшает counter на 1 и возвращает результат
  */
-export class Counter {}
+export class Counter {
+    counter: number;
+
+    constructor(start: number = 0) {
+        this.counter = start;
+    }
+
+    increment(): number {
+        return ++this.counter;
+    }
+    decrement(): number {
+        return --this.counter;
+    }
+}
